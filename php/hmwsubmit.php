56c5399eae2e9071499f70a9fc4aea4951047118
Chelsea Wu <cwu9@unl.edu>
2022-04-01 08:10:59 -0500

    add hmwsubmit php

---
+++ php/hmwsubmit.php
@@ -0,0 +1,7 @@
+<form action="post">
+        <p>
+            <input type="submit" name="dopost" value="Submit"/>
+            <input type="button" onclick="location.href='canvas.unl.edu/'; return false;"
+            value="Escape">
+        </p>
+    </form>
